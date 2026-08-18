21b057f3f2c438592dc5b9d566bb703b38a9692e
TaroUmeki <umeki.taro@gmail.com>
2026-08-18 14:35:04 +0900

    if系統

--- chapter4/main.js
+++ chapter4/main.js
@@ -4,3 +4,31 @@ if (isRaining) {
 } else {
   console.log("It's not raining, enjoy your day!");
 }
+
+const score = 75;
+if (score >= 90) {
+  console.log("You got an A!");
+} else if (score >= 80) {
+  console.log("You got a B!");
+} else if (score >= 70) {
+  console.log("You got a C!");
+} else {
+  console.log("You need to study more.");
+}
+
+const ok = true;
+const maybeOk = true;
+if (ok) {
+  console.log("Everything is fine.");
+} else if (maybeOk) {
+  console.log("Maybe everything is fine.");
+} else {
+  console.log("Something is wrong.");
+}
+
+const age = 20;
+if (age >= 20) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
