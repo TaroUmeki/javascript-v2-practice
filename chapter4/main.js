250ffe8b83856127738516a89acb3dc9289780d2
TaroUmeki <umeki.taro@gmail.com>
2026-08-18 15:29:38 +0900

    諸々

--- chapter4/main.js
+++ chapter4/main.js
@@ -32,3 +32,63 @@ if (age >= 20) {
 } else {
   console.log("You are a minor.");
 }
+
+console.log(1 === 1);
+console.log(1 === 2);
+
+console.log(1 !== 1);
+console.log(1 !== 2);
+
+const password = "1234";
+if (password === "1234") {
+  console.log("Access granted.");
+} else {
+  console.log("Access denied.");
+}
+
+const age2 = 18;
+if (age2 >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
+
+// how to differ object
+const dog1 = { name: "pocha" };
+const dog2 = { name: "pocha" };
+console.log(dog1 === dog2);
+console.log(dog1.name === dog2.name);
+
+const a = [1, 2, 3];
+const b = [1, 2, 3];
+console.log(a === b);
+console.log(a[0] === b[0]);
+
+const ok2 = "hello" && "はい";
+if (ok2) {
+  console.log("ok is true");
+}
+
+console.log(true && false);
+console.log(false || true);
+console.log(false || false);
+
+console.log("cat" && "dog");
+console.log(0 && "dog");
+console.log("cat" || "dog");
+console.log("" || "デフォルト");
+
+const input = "Taro";
+const result = input || "名無し";
+console.log(result);
+
+const result2 = 5 > 3 && 2 < 1;
+console.log(result2);
+
+const x = 15;
+console.log(x > 10 && x < 20);
+
+const x2 = 50;
+if (x2 >= 0 && x2 <= 100) {
+  console.log("x2 is between 0 and 100");
+}
